b3e9d1193458c3f60789bb98d6f05d14fa6a4a07
GabrielOliveiraSilvac <gabriel.osilvaccor@gmail.com>
2025-10-16 16:11:29 -0300

    Add files via upload
    Exercícios EPS 
    Primeiros Exercícios

---
+++ MediaNota.java
@@ -0,0 +1,12 @@
+
+public class MediaNota {
+	public static void main (String args[]) {
+	//declaração de variáveis
+	double nota1,nota2,media;
+	nota1=15;
+	nota2=20;
+	media=(nota1+nota2)/2;
+	System.out.println ("sua média é: "+media);
+
+	}
+}
