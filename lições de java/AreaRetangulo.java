c94e8cf617566ca8ed51b7a60f9c92f0ffe9803b
GabrielOliveiraSilvac <gabriel.osilvaccor@gmail.com>
2025-11-11 21:09:42 -0300

    Add files via upload

---
+++ lições de java/AreaRetangulo.java
@@ -0,0 +1,12 @@
+
+public class AreaRetangulo {
+	public static void main (String args[]) {
+		double peso,altura,imc;
+	peso=20;
+	altura=40;
+	imc=peso/(altura*altura);
+	System.out.println ("seu imc é:"+imc);
+	
+	}
+
+}
---
+++ lições de java/LataVolume.java
@@ -0,0 +1,13 @@
+
+public class LataVolume {
+	public static void main (String args[]) {
+		//declaração de variaveis
+		double raio,altura,volume;
+		raio=55;
+		altura=34;
+		volume=3.14*(raio*raio)*altura;
+		System.out.println ("o volume é:"+volume);
+		
+	}
+
+}
