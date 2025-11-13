ac87207155539ecc37367aae65791930d520bd47
GabrielOliveiraSilvac <gabriel.osilvaccor@gmail.com>
2025-11-13 20:32:01 -0300

    Add files via upload

---
+++ ExerVetor/exer14/Vetor14.java
@@ -0,0 +1,26 @@
+
+public class Vetor14 {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5,6,7,8,9,10};
+		int b[] = new int [10];
+		int i;
+		
+		//Separação de Valores Impar e Par A
+		System.out.println("Adicionando");
+		System.out.print("\n");
+		for(i=0;i<=10;i++) {
+			if (a[i]%2==0) {
+	            System.out.println("O numero do Vetor A é par: " +a[i]);
+	            b[i]=+1;
+	        } else {
+	            System.out.println("O numero do Vetor A é impar: " +a[i]);
+	            b[i]=+0;
+	        }
+			System.out.println("Vetor b Recebe: " +b[i]);
+		}
+		
+
+	}
+
+}
