81cac5fdd7227162c77a8c5c889f70c1fc2c620c
GabrielOliveiraSilvac <gabriel.osilvaccor@gmail.com>
2025-11-14 16:09:52 -0300

    Update VetorPotenciado.java

--- ExerVetor/exer6/VetorPotenciado.java
+++ ExerVetor/exer6/VetorPotenciado.java
@@ -1,20 +1,19 @@
 
 public class VetorPotenciado {
-//verificar
+
 	public static void main(String[] args) {
-		//corrigir fazer base * coeficiente, nao uma ´potencia normal!
-		//talvez mandar assim mesmo
 		int a[] = {0,1,2,3,4,5,6,7,8,9,10};
 		int i, c = 1;
 		
-		System.out.println("Transformando em versão de Potência " );
+		System.out.println("Transformando em versÃ£o de PotÃªncia " );
 		for(i=0;i<=a[i];i++) {
 			System.out.print("Numero: " +a[i]);
 			a[i] = c*i*a[i];
-			System.out.println(" Potência: " +a[i]);
+			System.out.println(" PotÃªncia: " +a[i]);
 		}
 		
 
 	}
 
 }
+
