82c3d77ffa0012d9a304494953d53890497eebdd
GabrielOliveiraSilvac <gabriel.osilvaccor@gmail.com>
2025-11-14 16:34:28 -0300

    Update VetorSomatorio.java

--- ExerVetor/exer8/VetorSomatorio.java
+++ ExerVetor/exer8/VetorSomatorio.java
@@ -3,19 +3,17 @@ public class VetorSomatorio {
 
 	public static void main(String[] args) {
 		int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int i, b, j=0;
+		int i, b;
 		
 		System.out.println("Somatorio");
-		for(i=0;i<=10;i++) {
+		for(i=0;i<a.length;i++) {
 			System.out.println("Valor a ser somado: " +a[i]);
-			b=j+a[i];
-			for(b=0;b<=a[i];b++) {
-				System.out.println("Somatorio de a: " +b);
-				
+			int j=0;
+			for(b=1;b<=a[i];b++) {
+				j+=b;
+				System.out.println("Somatorio de a: " +j);
 			}
 		}
-
-
 	}
 	
 }
